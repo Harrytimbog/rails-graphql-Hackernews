1732c25f3fd336ae42ed762a6482c4ba00cf774f
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-25 12:19:52 +0100

    Create some links in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_111254) do
+ActiveRecord::Schema.define(version: 2021_11_25_111438) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
