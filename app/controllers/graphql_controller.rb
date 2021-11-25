39e761ec5053904937be7720029bd75398486539
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-25 17:03:48 +0100

    Authentication(Linking users to created links and authenticating requests)

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -11,6 +11,8 @@ class GraphqlController < ApplicationController
     context = {
       # Query context goes here, for example:
       # current_user: current_user,
+      session: session,
+      current_user: current_user
     }
     result = RailsGraphqlTutorialSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -22,6 +24,19 @@ class GraphqlController < ApplicationController
   private
 
   # Handle variables in form data, JSON body, or a blank value
+  def current_user
+    # if we want to change the sign-in strategy, this is the place to do it
+    return unless session[:token]
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    user_id = token.gsub('user-id:', '').to_i
+    User.find user_id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
+
   def prepare_variables(variables_param)
     case variables_param
     when String
